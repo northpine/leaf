b0afc198f798dd399103091074493188194b0f83
Myles Haynes <myleschaynes@gmail.com>
2018-10-04 19:06:11 -0700

    Add redux chrome tools extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers';
 import {createLogger} from 'redux-logger';
 import thunk from 'redux-thunk';
-export default createStore(rootReducer, applyMiddleware(createLogger(), thunk));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export default createStore(rootReducer, composeEnhancers(applyMiddleware(createLogger(), thunk)));
