092a213ffd4fa15a73de26e06c98bbd65a4abdb1
Myles Haynes <myleschaynes@gmail.com>
2018-10-14 14:37:17 -0700

    remove unused import

--- src/components/Description.js
+++ src/components/Description.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import Popover from '@material-ui/core/Popover';
 import IconButton from '@material-ui/core/IconButton';
 import Description from '@material-ui/icons/Description';
