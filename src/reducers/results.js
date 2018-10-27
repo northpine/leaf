b7a396cbc3b939829b153c90cca03495b6a3193e
Myles Haynes <myleschaynes@gmail.com>
2018-10-27 16:50:34 -0700

    Better protect from null results

--- src/reducers/results.js
+++ src/reducers/results.js
@@ -5,22 +5,26 @@ export default (state = {}, action) => {
   switch(action.type) {
     case REPLACE_RESULTS:
 
-      const {features = []} = action.results;
+      const {features} = action.results;
       //We're going to group by server here.
       const serverList = {};
       const layerList = {};
-      features.sort().forEach((feature) => {
-        const layerUrl = feature.properties.url;
-        const baseUrl = shortenUrl(layerUrl);
-        if(!serverList[baseUrl]) {
-          serverList[baseUrl] = [];
-        }
-        layerList[layerUrl] = feature;
-        serverList[baseUrl].push({
-          url: layerUrl,
-          name: feature.properties.name
+      if(features) {
+        features.sort().forEach((feature) => {
+          if(feature && feature.properties) {
+            const layerUrl = feature.properties.url;
+            const baseUrl = shortenUrl(layerUrl);
+            if(!serverList[baseUrl]) {
+              serverList[baseUrl] = [];
+            }
+            layerList[layerUrl] = feature;
+            serverList[baseUrl].push({
+              url: layerUrl,
+              name: feature.properties.name
+            });
+          }
         });
-      });
+      }
 
       return Object.assign({}, {
         servers: serverList,
